05f6a2ab957a34ffad6535a3ef571476d6078cfd
Leonardo Maciel <leonardomacielj@gmail.com>
2025-07-08 20:36:04 -0300

    feat: added and configured authjs with prisma adpater, created models

---
+++ src/lib/auth.ts
@@ -0,0 +1,10 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+import GitHub from "next-auth/providers/github";
+import { prisma } from "./prisma";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [GitHub],
+});
