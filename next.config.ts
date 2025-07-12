265e73f871b4409d17064b62e9086c100a28f7e5
Leonardo Maciel <leonardomacielj@gmail.com>
2025-07-12 12:38:13 -0300

    feat: custom creator profile per user

--- next.config.ts
+++ next.config.ts
@@ -5,15 +5,19 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'github.com',
+        protocol: "https",
+        hostname: "github.com",
       },
       {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
       },
     ],
-  }
+  },
 };
 
 export default nextConfig;
